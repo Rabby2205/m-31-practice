0d6c408d6943cf5689e7b1714057c6ebeb2ebcb9
Rabby2205 <rabbyisalm1626@gmail.com>
2022-03-12 23:29:02 +0600

    class

---
+++ class-7.js
@@ -0,0 +1,9 @@
+class support {
+    name;
+    designation; 'support web dev';
+    startSession() {
+        console.log('start a support session');
+    }
+}
+const amir = new support();
+console.log(amir);
