17c186f23fbbec9d91ac164226b3b74a664f055c
Rabby2205 <rabbyisalm1626@gmail.com>
2022-03-12 21:50:57 +0600

    Comment changed

--- array-map.js
+++ array-map.js
@@ -1,8 +1,8 @@
-//array map is 
+//array map is an shortcut function to thorw loop for do some mechanism wiht arrays elements.
 const numbers = [4, 3, 6, 7, 10, 45, 2];
 const output = [];
 //old system or maual system
-const doubleIT = (number) => number * 2;
+const doubleIT = number => number * 2;
 
 for (number of numbers) {
     const result = doubleIT(number);
@@ -11,5 +11,5 @@ for (number of numbers) {
 // console.log(output);
 //now mapping system
 
-const output2 = numbers.map(doubleIT);
+const output2 = numbers.map(number => number * number);
 console.log(output2);
