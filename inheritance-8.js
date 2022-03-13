a2b1fd69d276e83a478f70230746865b8f3c9dae
Rabby2205 <rabbyisalm1626@gmail.com>
2022-03-13 21:23:55 +0600

    class-8

---
+++ inheritance-8.js
@@ -0,0 +1,37 @@
+//creating a class . 
+class Member {
+    name;
+    roll;
+    constructor(name, roll) {//this section is to take dynamic property .
+        this.name = name;
+        this.roll = roll;
+    }
+    startSession() {//this is doing something by funciton .
+        console.log(this.name, 'start a seasson');
+
+    }
+    designation = 'web Engineer';
+}
+
+// const kamal = new Member('kamal', 'web eng-2');
+// const Rabby = new Member('Rabby Islam', 'web Eng-1');
+// console.log(kamal);
+// console.log(Rabby);
+// Rabby.startSession();
+// kamal.startSession();
+
+class StudentCare {
+    name;
+    designation = 'Student care';
+    address;
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+    buildRoutin(student) {
+        console.log(this.name, 'Build a routin for', student);
+    }
+}
+const jamal = new StudentCare('jamal', 'Mumbai');
+console.log(jamal);
+jamal.buildRoutin('Sunnyleony');
